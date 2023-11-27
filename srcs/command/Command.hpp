a0bd45c420f652bd149f1065a803da49e9d8d141
Romain <romain.vlrt@gmail.com>
2023-11-27 16:40:51 +0100

    wip

--- srcs/command/Command.hpp
+++ srcs/command/Command.hpp
@@ -10,3 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef COMMAND_HPP
+# define COMMAND_HPP
+
+# include <string>
+
+class Command;
+
+class Command
+{
+    protected:
+
+        Server* _srv;
+        bool    _auth;
+
+        Command();
+        Command(const Command& src);
+
+    public:
+
+        // Sans explicit, cela pourrait être implicite
+        // Command cmd = &server; // Erreur si explicit est présent
+        // Avec explicit, doit être explicite
+        explicit Command(Server* srv, bool auth = true);
+        virtual ~Command();
+
+        bool auth_required() const;
+
+        virtual void execute(Client* client, std::vector<std::string> args) = 0;
+};
+
+class Join : public Command
+{
+    public:
+
+        Join(Server* srv);
+        ~Join();
+
+        void    execute(Client* client, std::vector<std::string> args);
+};
+
+#endif
--- srcs/command/Join.cpp
+++ srcs/command/Join.cpp
@@ -1,19 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Command.cpp                                        :+:      :+:    :+:   */
+/*   Join.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rovillar <rovillar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/08 04:49:56 by rovillar          #+#    #+#             */
-/*   Updated: 2023/11/27 16:17:18 by rovillar         ###   ########.fr       */
+/*   Created: 2023/11/27 16:37:34 by romain            #+#    #+#             */
+/*   Updated: 2023/11/27 16:40:28 by romain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Command.hpp"
 
-Command::Command(Server* srv, bool auth) :_srv(srv), _auth(auth) {}
+Join::Join(Server* srv) : Command(srv) {}
 
-Command::~Command() {}
+Join::~Join() {}
 
-bool    Command::auth_required() const { return _auth; }
+void    Join::execute(Client* client, std::vector<std::string> args) {
+    if (args.empty()) {
+    }
+}
